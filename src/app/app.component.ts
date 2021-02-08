68e35324d3768c28f7eccce5867349ea2e21e34f
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2021-02-08 23:31:14 +0300

    player arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tic-tac-toe';
+export class AppComponent implements OnInit {
+  player1: any;
+  player2: any;
+
+  player1Move = [];
+  player2Move = [];
+
+  ngOnInit() {
+
+  }
+
   ResetBoard() {
 
   }
